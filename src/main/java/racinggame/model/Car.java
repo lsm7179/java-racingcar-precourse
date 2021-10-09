6065a62c658bedfe5bcc0d1c042c02e310e79f95
이승민 <sml7179@naver.com>
2021-10-09 13:43:05 +0900

    feat: 차 이름을 부여하는 기능 추가

--- src/main/java/racinggame/model/Car.java
+++ src/main/java/racinggame/model/Car.java
@@ -2,10 +2,19 @@ package racinggame.model;
 
 public class Car {
 
+    private String name;
     private int distance = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
 
     public void go() {
         distance++;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int getDistance() {
--- src/test/java/racinggame/model/CarTest.java
+++ src/test/java/racinggame/model/CarTest.java
@@ -3,6 +3,8 @@ package racinggame.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -11,9 +13,19 @@ public class CarTest {
     @DisplayName("차가 전진하는 기능 검증")
     @Test
     void go() {
-        Car car = new Car();
+        Car car = new Car("");
         car.go();
 
         assertThat(car.getDistance()).isEqualTo(1);
     }
+
+    @DisplayName("차에 이름을 부여하는 기능 검증")
+    @ValueSource(strings = {"lsm","fox"})
+    @ParameterizedTest
+    void carNameInputTest(String name) {
+        Car car = new Car(name);
+
+        assertThat(car.getName()).isEqualTo(name);
+    }
+
 }
