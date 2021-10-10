0119e9829147a5d21e71e9f528ba6a5636b6ffe9
이승민 <sml7179@naver.com>
2021-10-10 23:57:08 +0900

    feat: PlayController 게임 제어 기능 추가

---
+++ src/main/java/racinggame/controller/PlayController.java
@@ -0,0 +1,61 @@
+package racinggame.controller;
+
+import nextstep.utils.Console;
+import racinggame.model.Cars;
+import racinggame.model.InputHandler;
+import racinggame.model.Winner;
+import racinggame.view.View;
+
+public class PlayController {
+
+    private InputHandler inputHandler;
+    private Cars cars;
+
+    public PlayController() {
+        this.inputHandler = new InputHandler();
+
+    }
+
+    public void start(){
+        View.printInputCarNames();
+        nameInput();
+        View.printInputCount();
+        play(countInput());
+    }
+
+    private void nameInput(){
+        try{
+            String carNames = Console.readLine();
+            cars = new Cars(inputHandler.makeCarNames(carNames));
+        }catch (IllegalArgumentException e){
+            View.print(e.getMessage());
+            nameInput();
+        }
+
+    }
+
+    private int countInput(){
+        try{
+            String count = Console.readLine();
+            return inputHandler.toInt(count);
+        }catch (IllegalArgumentException e){
+            View.print(e.getMessage());
+            return countInput();
+        }
+    }
+
+    private void play(int count){
+        View.printPlay();
+        while(count-- > 0){
+            cars.move();
+            View.print(cars.toString());
+        }
+        win();
+    }
+
+    private void win(){
+        Winner win = new Winner(cars);
+        View.print(win.toString());
+    }
+
+}
